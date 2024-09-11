c7179c1774e8727681ae0a2d53dee360db8403ca
grvPratik <pratikgauravv@gmail.com>
2024-09-12 00:14:56 +0530

    inital start

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+
+
+
+const app: Express = express();
+app.use(express.json());
+
+app.use(cors<Request>());
+
+
+
+app.get("/", async (req: Request, res: Response) => {
+    try {
+        res.status(200).send("get request here");
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Internal Server Error");
+    }
+    return;
+});
+
+// app.use(errorMiddleware);
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server 💽 listening on PORT ${process.env.PORT}`);
+});
+
+export default app;
