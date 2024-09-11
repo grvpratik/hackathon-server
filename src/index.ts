a4d9f7866e1960a8738d2c3196f8fa88bc471937
grvPratik <pratikgauravv@gmail.com>
2024-09-12 00:28:51 +0530

    nodemon

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import express, { Express, Request, Response } from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 
 
 
 const app: Express = express();
+
 app.use(express.json());
 
 app.use(cors<Request>());
