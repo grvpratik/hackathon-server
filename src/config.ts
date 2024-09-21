35d4a1ff68a6f1e48de163dee7bd32df32cc3cdb
grvPratik <pratikgauravv@gmail.com>
2024-09-21 17:18:13 +0530

    prisma update 2

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
-export const JWT_SECRET = process.env.JWT_SECRET ?? "pratikgrv";
-export const WORKER_JWT_SECRET = JWT_SECRET + "worker";
+// export const JWT_SECRET = process.env.JWT_SECRET ?? "pratikgrv";
+// export const WORKER_JWT_SECRET = JWT_SECRET + "worker";
 
 export const TOTAL_DECIMALS = 1000_000;
+
 
 // 1/1000_000_000_000_000_000
